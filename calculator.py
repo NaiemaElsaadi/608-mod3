885aa8d724ec7e0d8860b72ed7fbb922b006eb45
NaiemaElsaadi <111472237+NaiemaElsaadi@users.noreply.github.com>
2022-09-04 20:32:30 -0500

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,1 +1,7 @@
-
+# File 3 - calculator.py (Section 4.6, 4.7) "Naiema Elsaadi!!"
+import math
+math.sqrt(900)
+math.floor(9.2)
+print('the square root of 900 is', math.sqrt(900))
+print('the rounds 9.2 to the largest integer not greater than 9.2 is', math.floor(9.2))
+print('My name is Naiema Elsaadi')
--- population-dispersion.py
+++ population-dispersion.py
@@ -1,1 +1,12 @@
+# File 5 - population-dispersion.py (Section 4.18)  Naiema Elsaadi
+# by useing the statistics module to calculate measures of dispersion for a population. 
+# Using the data provided in 4.18 for 10 six-sided die rolls, and by calling functions in the statistics module - calculate:
+# Population variance using pvariance()
+# Population standard deviation using pstdev()
 
+import statistics
+statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
+statistics.pstdev([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
+print('This is Population variance of 10 six-sided die rolls', statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2]) )
+print('This is Population standard of 10 six-sided die rolls', statistics.pstdev([1, 3, 4, 2, 6, 5, 3, 4, 5, 2]) )
+print ('My name is Naiema Elsaadi')
